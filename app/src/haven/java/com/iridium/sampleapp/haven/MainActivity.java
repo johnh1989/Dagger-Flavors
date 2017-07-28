3439a3b78f882025a3070c94839d1dd549678362
john Hinton <jhinton@rapidsos.com>
2017-07-28 01:36:36 -0400

    update

--- app/src/haven/java/com/iridium/sampleapp/haven/MainActivity.java
+++ app/src/haven/java/com/iridium/sampleapp/haven/MainActivity.java
@@ -2,6 +2,9 @@ package com.iridium.sampleapp.haven;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.iridium.sampleapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = (TextView) findViewById(R.id.tv);
     }
 }
